2ace51260b011f2dcb7b52614bc23799c853575a
Mohammed Neundorf <mohammed.neundorf@codefrog-it.de>
2019-08-06 15:13:14 +0200

    fullfilled poc

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,35 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- **/
-
+import * as path from 'path';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import * as tree from 'directory-tree';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const port = process.env.port || 3333;
+
+  app.setGlobalPrefix(globalPrefix);
+  app.useStaticAssets({
+    root: path.join(__dirname, '../nxpkg'),
+    prefix: '/',
+  });
+
+  // const t = tree(path.resolve(__dirname, '..', '..', '..', '..'), {
+  //   exclude: [/node_modules/]
+  // });
+  // console.log(JSON.stringify(t, null, 4));
+
   await app.listen(port, () => {
-    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    console.log('Listening at http://localhost:' + port); // + '/' + globalPrefix);
   });
 }
 
